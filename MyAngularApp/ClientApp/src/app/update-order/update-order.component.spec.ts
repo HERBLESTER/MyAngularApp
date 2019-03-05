ecde6c5d1b5cae960499a823e44db6d4ea741f48
PaulMaudib <hltoyou@live.com>
2019-03-05 12:44:45 -0500

    Update Order FE Unit Test, fixed validation, added ids for testing

--- MyAngularApp/ClientApp/src/app/update-order/update-order.component.spec.ts
+++ MyAngularApp/ClientApp/src/app/update-order/update-order.component.spec.ts
@@ -11,16 +11,19 @@ import { OrderCompositeService } from '../services/order-composite.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { ActivatedRouteStub } from '../../testing/activated-route-stub';
 import { MetaDataServiceStub } from '../../testing/metadata-service-stub';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('UpdateOrderComponent', () => {
   let component: UpdateOrderComponent;
   let fixture: ComponentFixture<UpdateOrderComponent>;
-  let metaData: MetaData = { cities: [{ id: 1, name: 'Sarasota' }], operations: [{id: 1, name: 'Install'}], customers: [{ id: 1, name: 'Remax' }] };
+  let metaData: MetaData = { cities: [{ id: 1, name: 'Sarasota' }], operations: [{ id: 1, name: 'Install' }], customers: [{ id: 1, name: 'Remax' }] };
   const street: string = '123 Street';
   let order: Order;
 
   let orderCompositeService: any;
   let signalOrderUpdated: any;
+  let updateOrder: any;
+
   let router = {
     navigate: jasmine.createSpy('navigate')
   }
@@ -28,18 +31,28 @@ describe('UpdateOrderComponent', () => {
   beforeEach(async(() => {
     order = new Order();
     order.street = street;
+    order.cityId = metaData.cities[0].id;
+    order.cityName = metaData.cities[0].name;
+    order.customerId = metaData.customers[0].id;
+    order.customerName = metaData.customers[0].name;
+    order.operationId = metaData.operations[0].id;
+    order.operationName = metaData.operations[0].name;
+    order.dateReceived = new Date();
+    order.id = 1;
+    order.notes = 'a note';
 
     const orderDataService = jasmine.createSpyObj('OrderDataService', ['getOrder', 'updateOrder']);
     const getOrder = orderDataService.getOrder.and.returnValue(of(order));
-    const updateOrder = orderDataService.updateOrder.and.returnValue(of(order));
+    updateOrder = orderDataService.updateOrder.and.returnValue(of(order));
 
     orderCompositeService = jasmine.createSpyObj('OrderCompositeService', ['signalOrderUpdated']);
     signalOrderUpdated = orderCompositeService.signalOrderUpdated.and.returnValue(of(order));
-  
+
     TestBed.configureTestingModule({
       imports: [ReactiveFormsModule,
         ToastrModule.forRoot(),
-        RouterTestingModule.withRoutes([{ path: 'update-order/:id', component: UpdateOrderComponent }])],
+        RouterTestingModule.withRoutes([{ path: 'update-order/:id', component: UpdateOrderComponent }]),
+        BrowserAnimationsModule],
       declarations: [UpdateOrderComponent],
       providers: [
         { provide: OrderDataService, useValue: orderDataService },
@@ -49,7 +62,7 @@ describe('UpdateOrderComponent', () => {
         { provide: Router, useValue: router }
       ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(async(() => {
@@ -62,15 +75,83 @@ describe('UpdateOrderComponent', () => {
     expect(component).toBeTruthy();
   });
 
+  it('should reflect the order', () => {
+    const updateElement: HTMLElement = fixture.nativeElement;
+    let inputElement: HTMLInputElement = updateElement.querySelector('#status');
+    expect(inputElement.value).toEqual(Status[Status.Received]);
+
+    let selectElement: HTMLSelectElement = updateElement.querySelector('#customer');
+    expect(selectElement.value).toEqual(order.customerId.toString());
+
+    selectElement = updateElement.querySelector('#city');
+    expect(selectElement.value).toEqual(order.cityId.toString());
+
+    selectElement = updateElement.querySelector('#operation');
+    expect(selectElement.value).toEqual(order.operationId.toString());
+
+    inputElement = updateElement.querySelector('#street');
+    expect(inputElement.value).toEqual(order.street);
+
+    let textAreaElement: HTMLTextAreaElement = updateElement.querySelector('#notes');
+    expect(textAreaElement.value).toEqual(order.notes);
+
+    let buttonElement: HTMLButtonElement = updateElement.querySelector('#submit');
+    expect(buttonElement.disabled).toBeFalsy();
+
+    buttonElement = updateElement.querySelector('#cancelOrder');
+    expect(buttonElement.disabled).toBeFalsy();
+
+  });
+
   it('should Update Order', () => {
-    component.onSubmit();
+    order.street = '199 First Avenue';
+    const updateElement: HTMLElement = fixture.nativeElement;
+    let inputElement: HTMLInputElement = updateElement.querySelector('#street');
+    inputElement.value = order.street;
+    inputElement.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+
+    let buttonElement: HTMLButtonElement = updateElement.querySelector('#submit');
+    buttonElement.click();
+
+    expect(updateOrder).toHaveBeenCalledWith(order);
     expect(signalOrderUpdated).toHaveBeenCalledWith(order);
     expect(router.navigate).toHaveBeenCalledWith(['../']);
   });
 
- it('should Cancel Order', () => {
-    component.cancelOrder();
+  it('should Cancel Order', () => {
+    const updateElement: HTMLElement = fixture.nativeElement;
+    let buttonElement: HTMLButtonElement = updateElement.querySelector('#cancelOrder');
+    buttonElement.click();
     expect(component.isCanceled()).toBeTruthy();
+    let inputElement: HTMLInputElement = updateElement.querySelector('#status');
+    expect(inputElement.value).toEqual(Status[Status.Cancelled]);
+    fixture.detectChanges();
+
+    buttonElement = updateElement.querySelector('#cancelOrder');
+    expect(buttonElement.disabled).toBeTruthy();
+
+  });
+
+  it('should validate street', () => {
+    const updateElement: HTMLElement = fixture.nativeElement;
+    let inputElement: HTMLInputElement = updateElement.querySelector('#street');
+    inputElement.value = '123';
+    inputElement.dispatchEvent(new Event('input'));
+
+    fixture.detectChanges();
+
+    let divElement: HTMLDivElement = updateElement.querySelector('#streetMinLength');
+    expect(divElement.textContent).toEqual('Street must be at least 4 characters long.');
+
+    inputElement.value = '';
+    inputElement.dispatchEvent(new Event('input'));
+
+    fixture.detectChanges();
+
+    divElement = updateElement.querySelector('#streetRequired');
+    expect(divElement.textContent).toEqual('Street is required.');
+
   });
 
   it('should navigate on Cancel', () => {
